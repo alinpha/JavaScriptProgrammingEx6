36ead5726bb07130e87ef3e7f407dedb39f80111
Aline Vetrov <alinpha@gmail.com>
2020-02-04 12:15:43 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,1 +1,32 @@
-let instructores = new Array('Carter', 'Cusack', 'Murray', 'Ryan', 'Pieroni');
+let instructors = new Array('Carter', 'Cusack', 'Murray', 'Ryan', 'Pieroni');
+console.log(instructors[2]);
+
+console.log('----------------');
+
+
+instructors[3] = 'Burchill';
+console.log(instructors[3]);
+
+console.log('----------------');
+
+instructors.shift();
+for(let i=0; i<instructors.length; i++) {
+    console.log(instructors[i]);
+}
+
+console.log('----------------');
+
+instructors.unshift('Murphy');
+instructors.push('Croney');
+instructors.sort();
+for(let i in instructors) {
+    console.log(instructors[i]);
+}
+console.log('----------------');
+
+let instructorsCopy = instructors.slice(2,5);
+instructorsCopy.pop();
+instructorsCopy.forEach(x => {
+    console.log(x);
+});
+console.log('----------------');
